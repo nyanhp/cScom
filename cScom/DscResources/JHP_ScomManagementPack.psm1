84e6439f19cfe2856b4ac8f3162bcce33bb7360f
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-07-14 14:58:48 +0200

    MP resource

--- cScom/DscResources/JHP_ScomManagementPack.psm1
+++ cScom/DscResources/JHP_ScomManagementPack.psm1
@@ -18,33 +18,76 @@ class Reason
 class ScomManagementPack
 {
     [DscProperty(Key)] [System.String] $Name
-    [DscProperty(Mandatory)] [System.Management.Automation.PSCredential] $SCOMAdminCredential
-    [DscProperty(Mandatory)] [System.String] $SourceFilePath
+    [DscProperty()] [System.String] $ManagementPackPath
+    [DscProperty()] [System.String] $ManagementPackContent
     [DscProperty()] [Ensure] $Ensure = 'Present'
     [DscProperty(NotConfigurable)] [Reason[]] $Reasons
 
     [ScomManagementPack] Get()
     {
-        
-        return @{
-            Name = $this.Name
-            SCOMAdminCredential = $this.SCOMAdminCredential
-            SourceFilePath = $this.SourceFilePath
-            Ensure = $this.Ensure
-            Reasons = @{
-                Code = 'ScomManagementPack:ScomManagementPack:ManagementPackMissing'
-                Phrase = "Management pack $($this.Name) missing."
+        $reasonList = @()
+        $mp = Get-SCManagementPack -Name $this.Name
+
+        if ($null -eq $mp -and $this.Ensure -eq 'Present')
+        {
+            $reasonList += @{
+                Code   = 'ScomManagementPack:ScomManagementPack:ManagementPackNotFound'
+                Phrase = "No management pack with the name $($this.Name) was found."
             }
+        }
+
+        if ($null -ne $mp -and $this.Ensure -eq 'Absent')
+        {
+            $reasonList += @{
+                Code   = 'ScomManagementPack:ScomManagementPack:TooManyManagementPacks'
+                Phrase = "A management pack with the name $($this.Name) was found but ensure is set to absent."
+            }
+        }
+
+        return @{
+            Name                  = $mp.Name
+            ManagementPackPath    = $this.ManagementPackPath
+            ManagementPackContent = $this.ManagementPackContent
+            Ensure                = $this.Ensure
+            Reasons               = $reasonList
         }
     }
 
     [void] Set()
     {
+        if ($this.Ensure -eq 'Absent')
+        {
+            Get-SCManagementPack -Name $this.Name | Remove-SCManagementPack
+            return
+        }
 
+        if ($this.ManagementPackContent -and $this.ManagementPackPath)
+        {
+            throw ([ArgumentException]::new('You cannot use ManagementPackContent and ManagementPackPath at the same time.'))
+        }
+
+        if ($this.ManagementPackPath -and -not (Test-Path -Path $this.ManagementPackPath))
+        {
+            throw ([IO.FileNotFoundException]::new("$($this.ManagementPackPath) was not found."))
+        }
+
+        if ($this.ManagementPackContent)
+        {
+            $tmpPath = Join-Path -Path ([IO.Path]::GetTempPath()) -ChildPath "$($this.Name).xml"
+            $this.ManagementPackPath = New-Item -ItemType File -Path $tmpPath -Force
+            Set-Content -Path $tmpPath -Force -Encoding Unicode -Value $this.ManagementPackContent
+        }
+
+        if ($this.ManagementPackPath)
+        {
+            Import-SCManagementPack -FullName $this.ManagementPackPath
+        }
     }
 
     [bool] Test()
     {
-        return $true
+        $currentState = $this.Get()
+
+        return $($currentState.Reasons.Count -eq 0)
     }
 }
